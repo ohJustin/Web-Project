8ca0989aa51df31bdc7cc1032e99b4907e6f594a
Justin <jbcool02@yahoo.com>
2022-11-28 18:15:05 -0600

    Registration demo -error detecting match usernames

--- RForm.php
+++ RForm.php
@@ -1,6 +1,66 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
+
+function getPostback(){
+    $postback = trim($_SERVER["PHP_SELF"]);
+    $postback = htmlspecialchars($postback);
+    
+    return $postback;
+}
+function getDSN(){
+    //$dsn = "mysql:host=localhost;dbname=test";
+    $dsn = "mysql:host=localhost;port=8889;dbname=blogsite";
+    return $dsn;
+}
+
+function getUsername(){
+    $username = "root";
+    return $username;
+}
+//getPassword Function
+function getPassword(){
+    $password = "root";
+    return $password;
+}
+function getPDO(){
+    $dsn = getDSN();
+    $user = getUsername();
+    $psw = getPassword();
+    $pdo = new PDO($dsn,$user,$psw);
+    return $pdo;
+}
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    //Grab values submitted by registration page.
+    $username = $_POST["usr"];
+    $password = $_POST["pwd"];
+   
+
+    //Hash password
+    $passwordHash = password_hash($password, PASSWORD_BCRYPT);
+    //$pdo = getPDO();
+    //$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $mysqli = new mysqli("localhost","root","root","blogsite",8889);
+
+    $sql = "INSERT INTO Users VALUES ('". $username . "', '$passwordHash', 'testuniversity')";
+    if($mysqli->query($sql)){
+        echo "<p>Your account has been created.</p>",
+             "<p><a href='LForm.php'>Login</a></p></html>";
+      die;
+    }
+    elseif($mysqli->errno == 1062){
+        echo "<p>The username <strong>$username</strong> already exists.",
+         "Please choose another.</p>";
+    }
+    else{
+        die("Error ($mysqli->errno) $mysqli->error");
+    }
+
+}
+
+
 ?>
 
 <!DOCTYPE html>
